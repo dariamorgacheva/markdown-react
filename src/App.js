e33f70d06030f32f70c4f94e22125aa70436e800
Dasha Bataeva <dora.bataeva@gmail.com>
2022-03-31 10:26:18 -0400

    markdown, basic styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import "./App.css";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import ReactMarkdown from 'react-markdown'
 
 function App() {
+  const [text, setText] = useState('');
+  // const [message, setMessage] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+ 
+
+    <div className='container'>
+      <h1 className='text-center'>Markdown</h1>
+      <div className='flex'>
+        <div>
+        <h3 className='text-center'>Editor</h3>
+          <textarea 
+            className="input-style"
+            id="editor"
+            type="text" 
+            onChange={(event) => setText(event.target.value)}
+            placeholder='Your Message'
+            />
+          {/* <button onClick={()=> setMessage(text)}>Submit</button> */}
+          
+        </div>
+        <div>
+        <h3 className='text-center'>Preview</h3>
+        <ReactMarkdown 
+            className='output-style'
+            // id='preview'
+            //  dangerouslySetInnerHTML={{
+            //       __html: text,
+                // }}
+                >{text}</ReactMarkdown>
+        </div>
+      </div>
+      
+      </div>
+     </div>
+)
+};
+
 
 export default App;
